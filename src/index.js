aff8e2546a8961feaa2c38e3fad78eaba9e6489b
pbabu01t <pbabu01t@knowledgenettest.onmicrosoft.com>
2024-11-14 14:52:59 +0530

    Todo List

--- src/index.js
+++ src/index.js
@@ -2,6 +2,17 @@ const express = require("express")
 
 app = express()
 
+
+// TODO
+// 1. Add sample middleware and test it out
+// 2. Add sample html files and test it out
+// 3. Add Db connection logic and test it out
+// 4. Add sample models and test it out
+// 5. Integrate third-party APIs and test it out
+// 6. Connect to Azure Cloud and access the Blob Files
+// ...
+
+
 app.get("/", (req, res) => {
     return res.send("Hello World!")
 })
