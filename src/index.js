bbf4d6d902ea1986190a064d581c321441f7cae2
pbabu01t <pbabu01t@knowledgenettest.onmicrosoft.com>
2024-11-14 14:43:45 +0530

    Express - Base Setup

--- src/index.js
+++ src/index.js
@@ -1,2 +1,11 @@
+const express = require("express")
 
-console.log("helllo world!")
+app = express()
+
+app.get("/", (req, res) => {
+    return res.send("Hello World!")
+})
+
+app.listen(3000, () => {
+    console.log("App is Up and Serving on 3000")
+})
